2806e3b5aed934ca231eb3a6ea487c11b28d5129
guoliuyong <1983627726@qq.com>
2022-03-03 20:17:25 +0800

    feat: 用户列表,信息更新完成

--- Routes/article.js
+++ Routes/article.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-22 16:23:39
- * @LastEditTime: 2022-02-22 16:28:21
+ * @LastEditTime: 2022-03-03 19:25:58
  * @LastEditors: LAPTOP-L472H14P
  * @Description: In User Settings Edit
  * @FilePath: \blog_backStageSystem\blog_serve\Routes\article.js
@@ -11,7 +11,7 @@ const path = require('path')
 const db = require(path.join(__dirname, '../Model/model'))
 const article = express.Router();
 article.get("/article/list", (req,res)=>{
-    const querySql = "SELECT * from article_list";
+    const querySql = "SELECT * from article_list ";
     db(querySql,(err, result)=>{
       res.send({
           content: result
--- Routes/auth.js
+++ Routes/auth.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-28 09:45:33
- * @LastEditTime: 2022-03-03 11:26:09
+ * @LastEditTime: 2022-03-03 20:17:04
  * @LastEditors: LAPTOP-L472H14P
  * @Description: In User Settings Edit
  * @FilePath: \blog-system-serve\Routes\auth.js
@@ -24,6 +24,46 @@ auth.get('/self', (req, res) => {
     res.status(200).json({
       ...reuslt[0],
     })
+  })
+})
+auth.get('/userlist', (req, response) => {
+  const { pageSize, pageNum = 10 } = req.query
+  const startIndex = (pageSize - 1) * pageNum
+  const endIndex = Number(startIndex) + Number(pageNum)
+  const querySql = `select * from user_info LIMIT ${startIndex}, ${pageNum}`
+  db(querySql, (err, reuslt = []) => {
+    const totalSql = `SELECT COUNT(*) AS total FROM user_info  `
+    db(totalSql, (err, res) => {
+      response.status(200).json({
+        total: res[0].total,
+        content: reuslt.map((d) => {
+          const item = d
+          delete item.password
+          return item
+        }),
+      })
+    })
+  })
+})
+auth.post('/update/userlist', (req, res) => {
+  const { userId, telphone, nickname, email, realname, hobby,sex ,area} = req.body;
+  const updateSql = `
+   UPDATE user_info SET
+   telphone = '${telphone ? telphone : null}',
+   nickname = '${nickname}', 
+   email = '${email}', 
+   realname = '${realname}', 
+   hobby = '${hobby}',
+   sex = '${sex}',
+   area = '${area}'
+   WHERE userId = ${userId}`;
+  db(updateSql, (err, reuslt) => {
+    console.log(err);
+    if (reuslt.affectedRows === 1) {
+      res.status(200).json({
+        ...req.body,
+      })
+    }
   })
 })
 module.exports = auth
